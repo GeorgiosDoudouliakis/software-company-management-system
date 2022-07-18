824685931898f59fc817dddd61f0c1b5a8f921bd
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-07-18 22:23:28 +0300

    feat: Add a required input on sc-select which decides if select option is multiple or not

--- src/app/shared/modules/sc-select/sc-select.component.ts
+++ src/app/shared/modules/sc-select/sc-select.component.ts
@@ -2,7 +2,7 @@ import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {Option} from "../../models/select-option.model";
 
 @Component({
-  selector: 'sc-select[scLabel][scOptions][scWidth]',
+  selector: 'sc-select[scLabel][scOptions][scWidth][scMultiple]',
   templateUrl: './sc-select.component.html',
   styleUrls: ['./sc-select.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -11,6 +11,7 @@ export class ScSelectComponent implements OnInit {
   @Input() public scLabel: string;
   @Input() public scOptions: Option[];
   @Input() public scWidth: number;
+  @Input() public scMultiple: boolean;
 
   constructor() { }
 
