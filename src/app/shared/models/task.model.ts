a08987c972c784cc9988f1f4bc07b31543107a2b
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-07-23 21:53:55 +0300

    refactor: Remove projectId property

--- src/app/shared/models/task.model.ts
+++ src/app/shared/models/task.model.ts
@@ -3,7 +3,6 @@ export type TaskStatus = 'To Do' | 'In Progress' | 'Done';
 export interface Task {
   id: number;
   title: string;
-  projectId: number;
   estimation: string; // TODO type
   status: TaskStatus;
   dueDate: string; // TODO type
