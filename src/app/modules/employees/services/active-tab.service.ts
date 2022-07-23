97e540b134561ca373f58a96310cdd5ebc63af1f
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-07-23 18:24:27 +0300

    refactor: Show the appropriate tab depending on the existance of employees

--- src/app/modules/employees/services/active-tab.service.ts
+++ src/app/modules/employees/services/active-tab.service.ts
@@ -1,9 +1,11 @@
 import {Injectable} from '@angular/core';
 import {ActiveTab} from "../models/active-tab.model";
+import {EMPLOYEES} from "../components/list/employees.mock";
 
 @Injectable()
 export class ActiveTabService {
-  public activeTab: ActiveTab = 'list';
+  // TODO inject employees service to get employees length
+  public activeTab: ActiveTab = EMPLOYEES.length > 0 ? 'list' : 'create';
 
   constructor() { }
 }
